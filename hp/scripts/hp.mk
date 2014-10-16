3cfc1a56ca6e661dfea125431d2b1f08f8e4e2f1
Ivan <ivanlopes@42algoritmos.com.br>
2014-10-16 16:33:39 -0300

    Update

---
+++ hp/scripts/hp.mk
@@ -0,0 +1,20 @@
+SRCS      = $(shell ls *.hp50)
+
+EXT       = hpprg
+OUT       = $(foreach s,$(SRCS), $(addprefix $(s:.hp50=.), $(EXT)))
+
+PROGRAM   = $(SRCS:.hp50=.hpprg)
+TARGETS   = $(PROGRAM)
+
+all: $(TARGETS)
+
+%.hpprg: %.hp50
+	./$(SCRIPT) $^ > $@
+ide:
+	wine $(IDE)
+emu:
+	wine $(EMU)
+clean:
+	/bin/rm -rf $(TARGETS) *~ $(OUT)
+
+# END OF FILE
